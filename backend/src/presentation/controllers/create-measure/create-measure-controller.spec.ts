be1ac8458a329116773700a4753b442a607b2ff6
Rian Lucas <rianlucas2014@gmail.com>
2024-08-28 19:21:01 -0300

    test: create measure controller should call generative ai with correct value

--- backend/src/presentation/controllers/create-measure/create-measure-controller.spec.ts
+++ backend/src/presentation/controllers/create-measure/create-measure-controller.spec.ts
@@ -2,9 +2,33 @@ import { randomUUID } from "crypto"
 import { CreateMeasureController } from "./create-measure-controller"
 import { MeasureRepository } from "../../../domain/repositories/measure-repository"
 import { Measure } from "../../../domain/entities/measure"
+import { GenerativeAi } from "../../../data/protocols/generative-ai"
+import { base64ToFile } from "../../../utils/convert-image"
 
-const makeSut = () => {
-  return new CreateMeasureController(makeInMemoryRepository())
+interface SutTypes {
+  sut: CreateMeasureController,
+  generativeAi: GenerativeAi
+}
+
+const makeSut = (): SutTypes => {
+  const generativeAiStub = makeGenerativeAi()
+  const sut = new CreateMeasureController(makeInMemoryRepository(), generativeAiStub)
+
+  return {
+    sut,
+    generativeAi: generativeAiStub
+  }
+}
+
+const makeGenerativeAi = () => {
+  class GenerativeAiStub implements GenerativeAi {
+    async extractValueFromImage(image: File): Promise<string> {
+      return ""
+    }
+    
+  }
+
+  return new GenerativeAiStub()
 }
 
 const makeInMemoryRepository = () => {
@@ -28,8 +52,23 @@ const makeInMemoryRepository = () => {
 }
 
 describe('CreateMeasureController', () => {
+  it('should call generative ai with correct values', async () => {
+    const {sut, generativeAi} = makeSut()
+    const extractValueFromImageSpy = jest.spyOn(generativeAi, 'extractValueFromImage')
+
+    const body = {
+      image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+      customer_code: randomUUID().toString(),
+      measure_datetime: '2011-10-05T14:48:00.000Z',
+      measure_type: 'GAS'
+    }
+   
+    await sut.handle(body)
+
+    expect(extractValueFromImageSpy).toHaveBeenCalledWith(base64ToFile(body.image))
+  })
   it('should return 200 if valid data is provided', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       image: "valid_image",
@@ -38,13 +77,21 @@ describe('CreateMeasureController', () => {
       measure_type: 'GAS'
     }
 
-    const response = await sut.handle(body)
+    const body2 = {
+      image: "valid_image",
+      customer_code: randomUUID().toString(),
+      measure_datetime: '2011-10-05T14:48:00.000Z',
+      measure_type: 'WATER'
+    }
+
+    await sut.handle(body)
+    const response = await sut.handle(body2)
 
     expect(response.statusCode).toBe(200)
   })
 
   it('should return 409 if already exists a measure of provided measure type for the month provided', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       image: "valid_image",
@@ -68,7 +115,7 @@ describe('CreateMeasureController', () => {
   })
 
   it('should return 400 if invalid measure type is provided', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       image: "valid_image",
@@ -83,7 +130,7 @@ describe('CreateMeasureController', () => {
   })
 
   it('should return 400 if invalid measure type is provided', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       image: "valid_image",
@@ -98,7 +145,7 @@ describe('CreateMeasureController', () => {
   })
 
   it('should return 400 if missing image param', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       customer_code: randomUUID().toString(),
@@ -112,7 +159,7 @@ describe('CreateMeasureController', () => {
   })
 
   it('should return 400 if missing customer_code param', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       image: "valid_image",
@@ -126,7 +173,7 @@ describe('CreateMeasureController', () => {
   })
 
   it('should return 400 if missing measure_datetime param', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       customer_code: randomUUID().toString(),
@@ -140,7 +187,7 @@ describe('CreateMeasureController', () => {
   })
 
   it('should return 400 if missing measure_type param', async () => {
-    const sut = makeSut()
+    const {sut} = makeSut()
 
     const body = {
       customer_code: randomUUID().toString(),
